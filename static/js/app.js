ff72d12168da0b28f978d3e3e192f43e3bfbce93
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2014-02-26 17:45:21 +0000

    Grab services list and display

--- static/js/app.js
+++ static/js/app.js
@@ -55,4 +55,45 @@ playlistButton.addEventListener('click', function () {
   );
   console.log('Loading...');
 
-})
+});
+
+var streams = document.querySelector('.streams');
+
+xhr = createCORSRequest('GET', 'http://bbcservices.herokuapp.com/services.json');
+xhr.withCredentials = true;
+xhr.onload = function() {
+ var json = JSON.parse(xhr.responseText);
+ streams.innerHTML = json.services.map(function (service) {
+  if (service.streams.length > 0) {
+    return '<li><a href="' + service.streams[0].url + '">' + service.title + '</a></li>';
+  } else {
+    return '';
+  }
+ }).join('');
+};
+xhr.send();
+
+
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
