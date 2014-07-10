bfbeec7d7419376545c4bd20c99dc4684e8a54ff
Dan Nuttall <pixelblend@gmail.com>
2014-07-10 12:26:54 +0100

    Add MDNS discovery of players

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express  = require('express'),
     app      = express(),
     radiodan = require('radiodan-client'),
+    mdns     = require('./lib/mdns'),
     port     = 3001;
 
 app.use('/radiodan', radiodan.middleware());
@@ -8,5 +9,7 @@ app.use('/radiodan', radiodan.middleware());
 app.listen(port);
 
 app.use(express.static(__dirname + '/static'));
+
+mdns.advertise(radiodan, port);
 
 console.log('Listening on port '+port);
