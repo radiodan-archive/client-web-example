8e19ffc509e730bfaabff33970054af76b243686
Libby Miller <libby@nicecupoftea.org>
2014-12-05 14:45:05 +0000

    Removing Avahi and mdns for now

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var express  = require('express'),
     app      = express(),
     radiodan = require('radiodan-client'),
-    mdns     = require('./lib/mdns'),
     port     = process.env.PORT || 5000;
 
 app.use('/radiodan',
@@ -11,7 +10,5 @@ app.use('/radiodan',
 app.listen(port);
 
 app.use(express.static(__dirname + '/static'));
-
-mdns.advertise(radiodan, port);
 
 console.log('Listening on port '+port);
