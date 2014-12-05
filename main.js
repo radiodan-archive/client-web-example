f709aa2bf5280cd0450ca07cde1707eca0933b12
Libby Miller <libby@nicecupoftea.org>
2014-12-05 16:35:56 +0000

    Non-working adding to playlist

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var express  = require('express'),
     request = require('request'),
+    extractValues = require('extract-values'),
     app      = express(),
-    radiodan = require('radiodan-client'),
+    radiodanClient = require('radiodan-client'),
+    radiodan = radiodanClient.create(),
+    player = radiodan.player.get("default"),
     port     = process.env.PORT || 5000;
 
 app.use('/radiodan',
-  radiodan.middleware({crossOrigin: true})
+  radiodanClient.middleware({crossOrigin: true})
 );
 
 app.listen(port);
@@ -13,8 +16,24 @@ app.listen(port);
 request.get('https://huffduffer.com/libbymiller/rss', function (error, response, body) {
     if (!error && response.statusCode == 200) {
         var rss = body;
-        console.log(body);
-        // Continue with your processing here.
+
+        ///hack-parse the xml
+        var last_url = null;
+        var arr = body.split("\n")
+        for(var i =0; i< arr.length; i++){
+            var result = extractValues(arr[i], "url=\"{url}\" type=\"{type}\" length=\"{len}\"")
+            if(result){
+              var url = result["url"]
+              last_url = url;
+              console.log("adding url "+url);
+              player.add({ clear: true, playlist: url});
+            }
+        }
+        console.log(player);
+        if(last_url){
+          console.log("playing");
+          player.play();
+        }
     }
 });
 
