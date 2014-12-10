0e15146a0221bad50b885a7495dffe00586bedd4
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2014-12-10 10:44:37 +0000

    Read config file on startup (and write if doesn't exist)

--- main.js
+++ main.js
@@ -5,12 +5,16 @@ var express  = require('express'),
     radiodanClient = require('radiodan-client'),
     radiodan = radiodanClient.create(),
     player   = radiodan.player.get('main'),
+    fs = require('fs'),
     port     = process.env.PORT || 5000;
-
-// var proxy = require('express-http-proxy');
 
 app.use('/radiodan',
   radiodanClient.middleware({crossOrigin: true})
+);
+
+var config = readOrCreateConfigWithDefaults(
+  './config.json',
+  { feedUrl: 'https://huffduffer.com/libbymiller/rss' }
 );
 
 app.listen(port);
@@ -29,13 +33,12 @@ function stopPlaying() {
   player.pause({ value: true });
 }
 
-var newurl = 'https://huffduffer.com/libbymiller/rss';
-
 var powerButton = radiodan.button.get("power");
 powerButton.on("press", stopPlaying);
 powerButton.on("release", startPlaying);
 
-request(newurl, function (err, data) {
+console.log('Reading feedUrl', config.feedUrl);
+request(config.feedUrl, function (err, data) {
   var doc = cheerio(data.body);
   var urls = doc.find('enclosure')
                 .map(extractUrlFromEnclosure)
@@ -49,6 +52,19 @@ request(newurl, function (err, data) {
 function gracefulExit() {
   console.log('Exiting...');
   player.clear().then(process.exit);
+}
+
+function readOrCreateConfigWithDefaults(path, defaults) {
+  if ( fs.existsSync(path) ) {
+    return require(path);
+  } else {
+    writeConfig(path, defaults);
+    return defaults;
+  }
+}
+
+function writeConfig(path, config) {
+  fs.writeFileSync(path, JSON.stringify(config));
 }
 
 process.on('SIGTERM', gracefulExit);
